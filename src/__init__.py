944cdc6f89ea5b5cc1bd1e1391542e5517412428
Giuseppe Di Palma <dipalmagiuseppe1@gmail.com>
2021-05-25 14:43:25 +0200

    First commit :airplane:

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""This module provides only package."""
+
+__version__ = "0.1.0"
---
+++ src/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""This module provides TSS application."""
+
+import sys
+import snap
+
+def main():
+    print("hello ciao")
